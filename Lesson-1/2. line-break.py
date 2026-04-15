35972528e3a74d88d4fb690fd74573206d95ae93
JoySarkarBD <developer.joysarkar@gmail.com>
2026-04-15 10:42:41 +0600

    Add variables demonstration script

--- Lesson-1/2. line-break.py
+++ Lesson-1/2. line-break.py
@@ -1,2 +1,3 @@
 # console - Line Breaks
 print("Here is a line break\nAnd here is another one")
+
---
+++ Lesson-1/3. variables.py
@@ -0,0 +1,12 @@
+# Variables
+# A variable is a container for storing data values. In Python, you can create a variable by assigning a value to it using the equals sign (=).
+# Example:
+x = 5
+y = "Hello, World!"
+print(x)  # Output: 5
+print(y)  # Output: Hello, World!
+
+# In this example, we have created two variables: x, which holds the integer value 5, and y, which holds the string value "Hello, World!".
+# You can also change the value of a variable after it has been created. For example:
+x = 10
+print(x)  # Output: 10
